81683a48acb688545ca436990b474ce450648f3f
Aziz Yulianto <93021590+GeForce01@users.noreply.github.com>
2024-02-22 15:59:13 +0700

    upload homework (1) file

---
+++ pr.1.py
@@ -0,0 +1,55 @@
+print("=========================")
+print("pr. 1")
+siswa=["Budi","Charlie","Erlangga"]
+print(siswa)
+
+print("=========================")
+print("Remove Budi")
+siswa.remove("Budi")
+print(siswa)
+
+print("=========================")
+print("append Farel")
+siswa.append("Farel")
+print(siswa)
+
+print("=========================")
+print("len siswa")
+total=len(siswa)
+print(total)
+
+print("=========================")
+print("indeks Farel")
+siswa.index("Farel")
+print(siswa)
+
+print("=========================")
+print("insert 0,1 ")
+siswa.insert(0,1)
+print(siswa)
+
+print("=========================")
+print("pop 1")
+siswa.pop(1)
+print(siswa)
+
+print("=========================")
+print("count Farel")
+siswa.count("Farel")
+print(siswa)
+
+print("=========================")
+print("sort")
+angka=[12,3,24,1,23]
+angka.sort()
+print(angka)
+
+print("=========================")
+print("reverse")
+print(siswa)
+
+print("=========================")
+print("extend")
+print(siswa)
+
+
